adfd28f5d2cfce618db687b1e98b1804c556376b
RibinRaju <143781274+RibinRaju@users.noreply.github.com>
2024-08-14 08:55:22 +0530

    Delete ProjectManagement-app directory

--- ProjectManagement-app/src/main/java/com/example/projectmanagementapp/repository/ProjectRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.projectmanagementapp.repository;
-import org.springframework.stereotype.Repository;
-
-import com.example.projectmanagementapp.model.Project;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-@Repository
-public interface ProjectRepository extends JpaRepository<Project, Long> {
-}
